19f22bd9adf362114db05e3a0a96a252640a3321
AlgonzaDJun <algonsadewangga@gmail.com>
2023-11-29 22:42:05 +0700

    responsive page, card conselor, add props

---
+++ src/pages/PemesananPage/PemesananPage.jsx
@@ -0,0 +1,69 @@
+import CardConselor from "./CardConselor";
+
+const PemesananPage = () => {
+  return (
+    <div className="w-full min-h-screen p-4">
+      <div
+        id="cardContainer"
+        className="flex flex-wrap mx-auto justify-center lg:justify-start"
+      >
+        <CardConselor
+          namaKonselor={"udin"}
+          hargaKonselor={40000}
+          ratingKonselor={5}
+          imgKonselor={
+            "https://images.westend61.de/0001544702pw/handsome-male-doctor-with-clipboard-standing-in-front-of-wall-GIOF12206.jpg"
+          }
+        />
+        <CardConselor
+          namaKonselor={"asep"}
+          hargaKonselor={45000}
+          ratingKonselor={3}
+        />
+        <CardConselor
+          namaKonselor={"bagus"}
+          hargaKonselor={30000}
+          ratingKonselor={2}
+        />
+        <CardConselor
+          namaKonselor={"udin"}
+          hargaKonselor={40000}
+          ratingKonselor={5}
+          imgKonselor={
+            "https://images.westend61.de/0001544702pw/handsome-male-doctor-with-clipboard-standing-in-front-of-wall-GIOF12206.jpg"
+          }
+        />
+        <CardConselor
+          namaKonselor={"asep"}
+          hargaKonselor={45000}
+          ratingKonselor={3}
+        />
+        <CardConselor
+          namaKonselor={"bagus"}
+          hargaKonselor={30000}
+          ratingKonselor={2}
+        />
+        <CardConselor
+          namaKonselor={"udin"}
+          hargaKonselor={40000}
+          ratingKonselor={5}
+          imgKonselor={
+            "https://images.westend61.de/0001544702pw/handsome-male-doctor-with-clipboard-standing-in-front-of-wall-GIOF12206.jpg"
+          }
+        />
+        <CardConselor
+          namaKonselor={"asep"}
+          hargaKonselor={45000}
+          ratingKonselor={3}
+        />
+        <CardConselor
+          namaKonselor={"bagus"}
+          hargaKonselor={30000}
+          ratingKonselor={2}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PemesananPage;
