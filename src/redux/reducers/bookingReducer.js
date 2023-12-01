4c0521fb51b77456eed326e1c29631f2db7e0a15
AlgonzaDJun <algonsadewangga@gmail.com>
2023-12-01 07:26:16 +0700

    booking reducer

---
+++ src/redux/reducers/bookingReducer.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+
+const initialState = {
+  booking: [],
+  isLoading: false,
+  isFulfilled: false,
+  isErrored: {},
+};
+
+function bookingReducer(state = initialState, action) {
+  // create booking
+  switch (action.type) {
+    case "POST_BOOKING_PENDING":
+      return {
+        ...state,
+        isLoading: true,
+        isFulfilled: false,
+        isErrored: false,
+      };
+    case "POST_BOOKING_REJECTED":
+      return {
+        ...state,
+        isLoading: false,
+        isErrored: action.payload,
+      };
+    case "POST_BOOKING_FULFILLED":
+      return {
+        ...state,
+        isLoading: false,
+        isFulfilled: true,
+        booking: action.payload,
+      };
+    default:
+      return state;
+  }
+}
+
+function postBookingPending() {
+  return {
+    type: "POST_BOOKING_PENDING",
+  };
+}
+
+function postBookingRejected(data) {
+  return {
+    type: "POST_BOOKING_REJECTED",
+    payload: data,
+  };
+}
+
+function postBookingFulfilled(booking) {
+  return {
+    type: "POST_BOOKING_FULFILLED",
+    payload: booking,
+  };
+}
+
+export function postBooking(data) {
+  return async function (dispatch) {
+    dispatch(postBookingPending());
+    try {
+      const response = await axios.post(
+        `https://incare-backend-production.up.railway.app/booking`,
+        data
+      );
+      dispatch(postBookingFulfilled(response.data));
+    } catch (error) {
+      dispatch(postBookingRejected(error));
+    }
+  };
+}
+
+export default bookingReducer;
